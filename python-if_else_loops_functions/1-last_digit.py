1cedcb7b6efaca46545958232142be263f768aea
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-23 10:58:39 -0400

    print info of the last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number
+if last < 0:
+    last = last * -1 % 10
+else:
+    last = last % 10
+
+if (last > 5):
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif (last < 6):
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {last} and is 0")
--- python-if_else_loops_functions/README.md
+++ python-if_else_loops_functions/README.md
@@ -1,1 +1,3 @@
 task 0: assign a random signed number to the variable number each time it is executed. Print whether the number stored in the variable number is positive or negative.
+
+task 1: Take a random number and print if the las digit is greater than 5, less than 6 and not 0 and if is 0
