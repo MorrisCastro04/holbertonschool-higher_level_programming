2b2adbd847172fe98d5935514f2dfb4c68a26892
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-24 15:27:27 -0400

    fix the case if they are no arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-print("{} arguments:".format(len(sys.argv) - 1))
-for index in range(1, len(sys.argv)):
-    print("{}: {}".format(index, sys.argv[index]))
+if len(sys.argv) == 1:
+    print("{} arguments.".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for index in range(1, len(sys.argv)):
+        print("{}: {}".format(index, sys.argv[index]))
