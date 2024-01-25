30af17313457b78cfb4622db5c8bf99e7e3d5a95
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-25 12:59:46 -0400

    add a line to add the path if it is not in the default python path

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+sys.path.append('/path/to/hidden_4')
+
 if __name__ == '__main__':
     import hidden_4
 
-names = dir(hidden_4)
-for name in names:
-    if name[:2] != "__":
-        print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
