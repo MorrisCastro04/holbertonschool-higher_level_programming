cdadfa7169a08971cc713aba448d6a3770396b54
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-25 15:54:59 -0400

    remove * in the import

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    import calculator_1 as calc
+    from calculator_1 import add, sub, mul, div
 if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
@@ -9,13 +9,13 @@ a = int(sys.argv[1])
 operator = sys.argv[2]
 b = int(sys.argv[3])
 if sys.argv[2] == "+":
-    print(f"{a} {operator} {b} = {calc.add(a, b)}")
+    print(f"{a} {operator} {b} = {add(a, b)}")
 elif sys.argv[2] == '-':
-    print(f"{a} {operator} {b} = {calc.sub(a, b)}")
+    print(f"{a} {operator} {b} = {sub(a, b)}")
 elif sys.argv[2] == '*':
-    print(f"{a} {operator} {b} = {calc.mul(a, b)}")
+    print(f"{a} {operator} {b} = {mul(a, b)}")
 elif sys.argv[2] == '/':
-    print(f"{a} {operator} {b} = {calc.div(a, b)}")
+    print(f"{a} {operator} {b} = {div(a, b)}")
 else:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
