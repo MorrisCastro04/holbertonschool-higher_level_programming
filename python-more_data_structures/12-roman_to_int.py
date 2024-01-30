6826eb33e43613005bd82907213981e9176d9cc5
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-30 13:57:39 -0400

    fix pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     count = 0
-    temp = 0
-    leng = len(roman_string)
-    dic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    for letter in range(leng):
-        if letter < leng - 1:
-            if dic[roman_string[letter]] < dic[roman_string[letter + 1]]:
-                temp = dic[roman_string[letter]]
-                pass
-        count += dic[roman_string[letter]] - temp
+    if roman_string:
+        temp = 0
+        leng = len(roman_string)
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        for letter in range(leng):
+            if letter < leng - 1:
+                if dic[roman_string[letter]] < dic[roman_string[letter + 1]]:
+                    temp = dic[roman_string[letter]]
+                    pass
+            count += dic[roman_string[letter]] - temp
     return (count)
