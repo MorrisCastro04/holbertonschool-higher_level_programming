53ef65a6c4d7a0904423d135100d93fc58c4efed
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-30 09:07:32 -0400

    chnage the funcion max to a code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    score = ''
+    max_score = 0
+    for value in a_dictionary:
+        if a_dictionary[value] > max_score:
+            max_score = a_dictionary[value]
+            score = value
+    return (score)
