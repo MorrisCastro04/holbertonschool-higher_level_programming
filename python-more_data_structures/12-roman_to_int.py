31a1090b9670aed58534edf7b87771b6f46de52d
MorrisCastro <jomarcastro2004@gmail.com>
2024-01-30 14:01:01 -0400

    check if is a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     count = 0
-    if roman_string:
+    if roman_string and type(roman_string) == str:
         temp = 0
         leng = len(roman_string)
         dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
