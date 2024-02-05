8ddc6046b78be18a99e4764e2358a3dac20138fe
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-05 10:12:42 -0400

    create a empty class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    create a class rectangle that is empty
+"""
+
+
+class Rectangle():
+    """
+        create a class rectangle that is empty
+    """
+    def __init__(self) -> None:
+        pass
