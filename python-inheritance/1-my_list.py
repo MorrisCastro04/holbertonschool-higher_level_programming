73fe10ffbd8fce64d11a93212124a23427741e04
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-06 13:15:32 -0400

    write class that inherits another class

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/binpython3
+"""
+    class that in inherits from list
+"""
+
+
+class MyList(list):
+    """
+        method to sort a list
+    """
+    def print_sorted(self):
+        print(sorted(self))
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,3 @@
 task 0: returns the list of available attributes and methods of an object
+
+task 1: write class that inherits another class
