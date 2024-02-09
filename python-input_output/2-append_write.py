e9695ca84bef4ef002f0406809dc03ffd2e797be
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-09 13:46:20 -0400

    leave 2 space between doc and code

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,6 +2,8 @@
 """
     append a string at the end of a file
 """
+
+
 def append_write(filename="", text=""):
     """open the file in append mode"""
     with open(filename, 'a', encoding='utf-8') as file:
