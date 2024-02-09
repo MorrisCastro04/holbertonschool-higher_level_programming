1cf630a82ad8738139dda872f2d9508c623f66ea
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-09 13:33:07 -0400

    append a string at the end of the file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    with open(filename, 'a+', encoding='utf-8') as file:
+        file.seek(0, 2)
+        file.write(text)
+        file.seek(0)
+        return len(text)
--- python-input_output/README.md
+++ python-input_output/README.md
@@ -1,3 +1,5 @@
 task 0: reads a text file utf-8 and print it
 
 task 1: writes a string to a text file
+
+task 2: append a string at the end of the file
