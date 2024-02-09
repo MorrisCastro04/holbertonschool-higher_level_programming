ed4ef53f9b0030505ff241d280cf583da5826806
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-09 13:35:19 -0400

    change a to wr

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
-    with open(filename, 'a+', encoding='utf-8') as file:
+    with open(filename, 'wr+', encoding='utf-8') as file:
         file.seek(0, 2)
         file.write(text)
         file.seek(0)
