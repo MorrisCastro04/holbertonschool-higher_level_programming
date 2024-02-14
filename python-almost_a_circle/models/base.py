188faa8f990058e501fe2957c337e8099746abf5
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-14 00:19:45 -0400

    change the empty string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,8 +26,8 @@ class Base():
     @classmethod
     def save_to_file(cls, list_objs):
         """checks if list_objs is empty"""
-        if list_objs is []:
-            return ""
+        if list_objs is None:
+            return []
         list_dict = []
         with open(cls.__name__ + ".json", 'w') as file:
             for obj in list_objs:
