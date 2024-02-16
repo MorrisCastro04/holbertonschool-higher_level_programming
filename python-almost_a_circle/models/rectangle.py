8e61d5630e5f0d5509c52ae050e4add5ca7100df
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-16 17:01:24 -0400

    update the metod update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,11 +92,21 @@ class Rectangle(Base):
         return string
 
     """update the class rectangle"""
-    def update(self, *args, **kargs):
+    def update(self, *args, **kwargs):
         """checks if the class have a atribute"""
-        for key, value in kargs.items():
-            if hasattr(self, key):
+        if len(args) == 0:
+            for key, value in kwargs.items():
                 setattr(self, key, value)
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) == 5:
+            self.__y == args[4]
 
     """convert to dictionary"""
     def to_dictionary(self):
