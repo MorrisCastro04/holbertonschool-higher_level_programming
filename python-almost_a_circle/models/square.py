a8ee8558198861d8a06d8fdd55a02053b1698dbd
MorrisCastro <jomarcastro2004@gmail.com>
2024-02-17 13:52:09 -0400

    fix update method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -29,17 +29,18 @@ class Square(Rectangle):
     """update the square"""
     def update(self, *args, **kwargs):
         """check if is going to use args or kwargs"""
-        if len(args) == 0:
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.size = args[1]
-        if len(args) >= 3:
-            self.x = args[2]
-        if len(args) == 4:
-            self.y = args[3]
 
     """convert to dictionary"""
     def to_dictionary(self):
